20a44b3e81e1bc985944adcfe6bfe26d8bb4f872
luckmer <woszczo04@gmail.com>
2021-08-31 21:36:06 +0200

    added custom scrollbar

--- src/css/Global/index.js
+++ src/css/Global/index.js
@@ -12,6 +12,23 @@ const GlobalStyle = createGlobalStyle`
     background: linear-gradient(222.26deg, #3b1d60 1.22%, #040d3e 106.28%);
   }
 
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+}
+
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(222.26deg, #3b1d60 1.22%, #040d3e 106.28%);
+  border-radius: 5px;
+}
+
+
+
+
 `;
 
 export default GlobalStyle;
