18744a8f55de4c79f324ad5ab115303c6ccba5f3
luckmer <woszczo04@gmail.com>
2021-08-31 21:30:37 +0200

    export panel

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
-export { default as NavBar } from "./NavBar";
 export { default as VisitPanel } from "./VisitPanel";
+export { default as Statistics } from "./statistics";
+export { default as Investors } from "./Investors";
+export { default as Business } from "./Business";
+export { default as NavBar } from "./NavBar";
 export { default as Footer } from "./Footer";
+export { default as Offers } from "./Offers";
+export { default as About } from "./About";
