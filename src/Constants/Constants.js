8be76d2ea74d9e0a173654fc8d13d6894079c195
luckmer <woszczo04@gmail.com>
2021-08-31 21:32:27 +0200

    reusable components

---
+++ src/Constants/Constants.js
@@ -0,0 +1,90 @@
+import horizontal from "../img/Flip-horizontal.png";
+import Attachment from "../img/Attachment#1.png";
+import Move from "../img/Thunder-move.png";
+import airPods from "../img/Airpods.png";
+import bulb from "../img/Bulb#1.png";
+import lte from "../img/LTE#1.png";
+
+export const start = "GET STARTED";
+
+export const offer = "Your choice";
+
+export const staticHeader = "Increase your business ";
+
+export const staticMain = "traffic";
+
+export const staticDesc =
+  "We are committed to processing the information in order to contact you and talk about your project.";
+export const staticBtn = "Learn More";
+
+export const offerDescription =
+  "There are many reasons to get down and start to get depressed about your situation.";
+
+export const initialText =
+  "Many reasons to get up and start to get back in the business";
+
+export const description =
+  "The harder you work for something, the greater you’ll feel when you achieve it.";
+
+export const watch = "Watch preview";
+
+export const userText = ["ABOUT", "GALLERY", "PRICING", "FAG", "BENEFITS"];
+export const loginText = ["SIGN IN ", "SIGN UP"];
+export const buttons = ["LEARN MORE", "DEMO"];
+
+export const businessHeader = "Invest property for better ";
+export const businessMain = "business";
+export const businessDesc =
+  "We are committed to processing the information in order to contact you and talk about your project. We are committed to processing the information.";
+
+export const aboutDesc =
+  "Fully customizable and neatly organized components will help you work faster without limiting creative freedom.";
+export const aboutStatistics = [
+  {
+    value: "100+",
+    desc: "5 Reasons To Purchase Desktop Computers",
+  },
+  {
+    value: "43,000+",
+    desc: "3 Simple Ways To Save A Bunch Of Money When Buying A New Computer",
+  },
+  {
+    value: "30+",
+    desc: "A Discount Toner Cartridge Is Better Than Ever And You Will Save 50 Or More",
+  },
+];
+
+export const offers = [
+  {
+    img: horizontal,
+    desc: "Article nor prepare chicken you him now. Shy merits say advice ten before lovers innate add.",
+    content: "Folly words widow",
+  },
+  {
+    img: airPods,
+    desc: "Effect if in up no depend seemed. Ecstatic elegance gay but disposed. We me rent been part what.",
+    content: "Ecstatic elegance",
+  },
+
+  {
+    img: lte,
+    desc: "We me rent been part what. An concluded sportsman offending so provision mr education.",
+    content: "Possible procured trifling",
+  },
+
+  {
+    img: Move,
+    desc: "Shy merits say advice ten before lovers innate add. She cordially behaviour can attempted estimable.",
+    content: "Open game",
+  },
+  {
+    img: Attachment,
+    desc: "Improve ashamed married expense bed her comfort pursuit mrs. Four time took ye your as fail lady.",
+    content: "Endeavor",
+  },
+  {
+    img: bulb,
+    desc: "Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug.",
+    content: "Comfort pursuit",
+  },
+];
