1f4837fbbb1cee9dd653ee1c736fbeaff5c61055
luckmer <woszczo04@gmail.com>
2021-08-31 21:33:23 +0200

    company offers

---
+++ src/components/Offers.jsx
@@ -0,0 +1,28 @@
+import { offers, offer, offerDescription } from "../Constants/Constants";
+import * as O from "../css/components/Offers.style";
+
+const Offers = () => {
+  return (
+    <O.Section>
+      <O.DivSpacer>
+        <O.P>{offer}</O.P>
+        <O.Span>{offerDescription}</O.Span>
+      </O.DivSpacer>
+      <O.OfferPanel>
+        {offers.map((name, i) => (
+          <O.OfferCard key={i}>
+            <O.OfferHeader>
+              <img src={name.img} alt="" />
+              <span>{name.content}</span>
+            </O.OfferHeader>
+            <div>
+              <O.OfferP>{name.desc}</O.OfferP>
+            </div>
+          </O.OfferCard>
+        ))}
+      </O.OfferPanel>
+    </O.Section>
+  );
+};
+
+export default Offers;
