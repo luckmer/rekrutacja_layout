c92510ae7f53224391d7b786971e53943b5485a2
luckmer <woszczo04@gmail.com>
2021-09-02 12:03:12 +0200

    added contact panel

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
       <Component.Statistics />
       <Component.About />
       <Component.Business />
+      <Component.ContactPanel />
       <Component.Footer />
     </Main>
   );
